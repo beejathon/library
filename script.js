1e68f89521932cda416c2cc8badcfe59e3c1fc31
Bee-jay <bjpaiz@gmail.com>
2021-09-10 20:44:27 +0100

    added modal popup form

--- script.js
+++ script.js
@@ -14,48 +14,81 @@ function addBookToLibrary(e) {
     myLibrary.push(book);
     updateLibrary();
     document.getElementById('bookregister').reset();
+
 }
 
 function updateLibrary() {
     bookshelf.textContent = '';
     myLibrary.forEach(book => {
-        const entry = document.createElement('p');
-        entry.classList.add('bookcard')
+        const entry = document.createElement('div');
         const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
         title.textContent = book.title;
         title.style.fontSize = 'large';
         title.style.fontWeight = 'bold';
-        entry.appendChild(title);
-        const author = document.createElement('p');
         author.textContent = 'Author: ' + book.author;
-        entry.appendChild(author);
-        const pages = document.createElement('p');
         pages.textContent = 'Pages: ' + book.pages;
-        entry.appendChild(pages)
+        entry.classList.add('bookcard')
+        entry.appendChild(title);
+        entry.appendChild(author);
+        entry.appendChild(pages);
         bookshelf.appendChild(entry);
     });
 }
 
-
+const bookshelf = document.querySelector('.bookshelf');
 const bookRegister = document.getElementById('bookregister');
 bookRegister.addEventListener('submit', addBookToLibrary);
-const bookshelf = document.querySelector('.bookshelf');
 
-const firstbook = new Book('The Wretched of the Earth', 'Frantz Fanon', '320');
-myLibrary.push(firstbook);
-myLibrary.forEach(book => {
-    const entry = document.createElement('p');
-    entry.classList.add('bookcard')
-    const title = document.createElement('p');
-    title.textContent = book.title;
-    title.style.fontSize = 'large';
-    title.style.fontWeight = 'bold';
-    entry.appendChild(title);
-    const author = document.createElement('p');
-    author.textContent = 'Author: ' + book.author;
-    entry.appendChild(author);
-    const pages = document.createElement('p');
-    pages.textContent = 'Pages: ' + book.pages;
-    entry.appendChild(pages)
-    bookshelf.appendChild(entry);
-});
+const first = new Book('Capital, Vol. 1: A Critical Analysis of Capitalist Production ', 'Karl Marx', '1152');
+myLibrary.push(first);
+const second = new Book('Imperialism: The Highest Stage of Capitalism', 'Vladimir Lenin', '192');
+myLibrary.push(second);
+const third = new Book('The State and Revolution', 'Vladimir Lenin', '116');
+myLibrary.push(third);
+const fourth = new Book('Socialism: Utopian and Scientific', 'Friedrich Engels', '86');
+myLibrary.push(fourth);
+const fifth = new Book('Quotations from Chairman Mao Tse-Tung', 'Mao Zedong', '311');
+myLibrary.push(fifth);
+const sixth = new Book('The Wretched of the Earth', 'Frantz Fanon', '320');
+myLibrary.push(sixth);
+updateLibrary();
+
+const openModalButtons = document.querySelectorAll('[data-modal-target');
+const closeModalButtons = document.querySelectorAll('[data-close-button');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
