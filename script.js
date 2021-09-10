859039c34e40d18a83db8aef2ba6822da768e4fb
Bee-jay <bjpaiz@gmail.com>
2021-09-10 16:42:29 +0100

    added form to enter new books

--- script.js
+++ script.js
@@ -6,20 +6,50 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary(title, author, pages) {
-    const book = new Book(title, author, pages);
+function addBookToLibrary(e) {
+    e.preventDefault();
+    const dataForm = new FormData(e.target);
+    console.log(dataForm.get('status'));
+    const book = new Book(dataForm.get('title'), dataForm.get('author'), dataForm.get('pages'));
     myLibrary.push(book);
+    updateLibrary();
+    document.getElementById('bookregister').reset();
 }
 
-const firstbook = new Book('The Wretched of the Earth', 'Frantz Fanon', '320');
-myLibrary.push(firstbook)
+function updateLibrary() {
+    bookshelf.textContent = '';
+    myLibrary.forEach(book => {
+        const entry = document.createElement('p');
+        entry.classList.add('bookcard')
+        const title = document.createElement('p');
+        title.textContent = book.title;
+        title.style.fontSize = 'large';
+        title.style.fontWeight = 'bold';
+        entry.appendChild(title);
+        const author = document.createElement('p');
+        author.textContent = 'Author: ' + book.author;
+        entry.appendChild(author);
+        const pages = document.createElement('p');
+        pages.textContent = 'Pages: ' + book.pages;
+        entry.appendChild(pages)
+        bookshelf.appendChild(entry);
+    });
+}
 
+
+const bookRegister = document.getElementById('bookregister');
+bookRegister.addEventListener('submit', addBookToLibrary);
 const bookshelf = document.querySelector('.bookshelf');
+
+const firstbook = new Book('The Wretched of the Earth', 'Frantz Fanon', '320');
+myLibrary.push(firstbook);
 myLibrary.forEach(book => {
     const entry = document.createElement('p');
     entry.classList.add('bookcard')
     const title = document.createElement('p');
-    title.textContent = 'Title: ' + book.title;
+    title.textContent = book.title;
+    title.style.fontSize = 'large';
+    title.style.fontWeight = 'bold';
     entry.appendChild(title);
     const author = document.createElement('p');
     author.textContent = 'Author: ' + book.author;
@@ -29,5 +59,3 @@ myLibrary.forEach(book => {
     entry.appendChild(pages)
     bookshelf.appendChild(entry);
 });
-
-
